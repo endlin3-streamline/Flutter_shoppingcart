5ae2f01e85f1f3b1d916b9cb3ab75c6469863821
endlin3-streamline <albertoey06@gmail.com>
2026-09-20 22:26:37 +0700

    publishthree

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _PageState extends State<ProfilePage> {
                         ),
                       ),
                       Text(
-                        '$name telah dipilihhh.',
+                        '$name telah dipilih',
                         style: const TextStyle(color: Colors.white70, fontSize: 12),
                       ),
                     ],
